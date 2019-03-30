b29bbd9482bf1a0b5c143eb4966cbdc57bf66baf
yibo.yu <yibo.yu@yaduo.com>
2019-03-30 22:08:12 +0800

    学习

--- src/main/java/com/xb/learn/Array.java
+++ src/main/java/com/xb/learn/Array.java
@@ -101,11 +101,36 @@ public class Array {
         return i;
     }
 
+    public static boolean isMatch(String s, String p) {
+        boolean[][] value = new boolean[p.length()+1][s.length()+1];
+        value[0][0] = true;
+        for(int i = 1;i <= s.length();i++){
+            value[0][i] = false;
+        }
+        for(int i = 1;i <= p.length(); i++){
+            if(p.charAt(i-1) == '*'){
+                value[i][0] = value[i-1][0];
+                for(int j = 1;j <= s.length(); j++){
+                    value[i][j] = (value[i][j-1] || value[i-1][j] || value[i-1][j-1]);
+                }
+            }else if(p.charAt(i-1) == '?'){
+                value[i][0] = false;
+                for(int j = 1;j <= s.length(); j++){
+                    value[i][j] = value[i-1][j-1];
+                }
+            }else {
+                value[i][0] = false;
+                for(int j = 1;j <= s.length(); j++){
+                    value[i][j] = s.charAt(j-1) == p.charAt(i-1) && value[i-1][j-1];
+                }
+            }
+
+        }
+        return value[p.length()][s.length()];
+    }
+
     public static void main(String[] args) {
 
-        int[] nums = {1,1,1,2,2,3};
-        removeDuplicates(nums);
-
-        System.out.println(Arrays.toString(nums));
+        System.out.println(isMatch("cb", "?a"));
     }
 }
